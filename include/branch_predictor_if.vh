dc7b10db452f62f999a2beef0a4f77c394cd17c6
Jiahao Xu <xu1392@purdue.edu>
2021-10-07 10:32:56 -0400

    fixing branch predictor

--- include/branch_predictor_if.vh
+++ include/branch_predictor_if.vh
@@ -4,22 +4,23 @@
 `include "cpu_types_pkg.vh"
 `include "dp_types_pkg.vh"
 
-interface branch_pred_if;
+interface branch_predictor_if;
     import cpu_types_pkg::*; 
     import dp_types_pkg::*; 
 
-    logic phit;
+    logic phit, loadEN; 
+    logic [1:0]
     word_t cpc, pc4, baddr, npc; 
 
     // still need CLK and nRST
     
     modport bp (
-        input   phit, pc4, baddr, 
-        output  npc 
+        input   phit, loadEN, buf_load, pc4, 
+        output  npc, wbuf 
     ); 
     modport tb (
-        output  phit, pc4, baddr, 
-        input   npc 
+        output  phit, pc4, loadEN, buf_load,
+        input   npc, wbuf 
     ); 
 endinterface
 `endif 
--- source/branch_predictor.sv
+++ source/branch_predictor.sv
@@ -5,16 +5,16 @@
 import cpu_types_pkg::*; 
 import dp_types_pkg::*; 
 
-module branch_pred(
+module branch_predictor(
     input logic CLK, nRST, 
-    branch_pred_if bpif
+    branch_predictor_if bpif
 );
     // design concept: latch the cpc as cpc_l, compare it with pc4 and baddr.
     //   
     branch_pred_state_t s, nxt_s; 
     always_ff @(posedge CLK, negedge nRST) begin
          if (~nRST) begin
-            s <= BPRED_NH;
+            s <= BPRED_NS;
 	 end
          // adding else condition for updating the state
          else begin
@@ -23,24 +23,27 @@ module branch_pred(
     end
     always_comb begin
         casez(s)
-        BPRED_NH: begin
-            nxt_s = (bpif.phit) ? BPRED_NS : BPRED_NH; 
-        end
-        BPRED_NS: begin
-            //nxt_s = (bpif.phit) ? BPRED_TH : BPRED_NH;
-	    nxt_s = (bpif.phit) ? BPRED_TS : BPRED_NH;
-        end
-        BPRED_TH: begin
-            nxt_s = (bpif.phit) ? BPRED_TH : BPRED_TS; 
-        end
-        BPRED_TS:begin
-            //nxt_s = (bpif.phit) ? BPRED_TH : BPRED_NH;
-	    nxt_s = (bpif.phit) ? BPRED_TH : BPRED_NS;
-	end
-        default: begin
-            nxt_s = BPRED_NH;
-	end
+            BPRED_NH: begin
+                nxt_s = (bpif.phit) ? BPRED_NS : BPRED_NH; 
+            end
+            BPRED_NS: begin
+                //nxt_s = (bpif.phit) ? BPRED_TH : BPRED_NH;
+	            nxt_s = (bpif.phit) ? BPRED_TS : BPRED_NH;
+            end
+            BPRED_TH: begin
+                nxt_s = (bpif.phit) ? BPRED_TH : BPRED_TS; 
+            end
+            BPRED_TS:begin
+                //nxt_s = (bpif.phit) ? BPRED_TH : BPRED_NH;
+	            nxt_s = (bpif.phit) ? BPRED_TH : BPRED_NS;
+	        end
+            default: begin
+                nxt_s = BPRED_NH;
+	        end
         endcase
+        if (bpif.loadEN) begin
+            nxt_s = bpif.buf_load;  
+        end
     end
 
     always_comb begin
