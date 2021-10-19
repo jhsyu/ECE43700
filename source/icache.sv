545d17b0bc4b2aa47c4f7d3e01ee3824d741abf3
Nate Nauman <nnauman@purdue.edu>
2021-10-18 20:22:25 -0400

    added basic memory access

--- source/icache.sv
+++ source/icache.sv
@@ -7,11 +7,13 @@ import dp_types_pkg::*;
 
 module icache (
   input CLK, nRST,
-  datapath_cache_if.icache icif
+  datapath_cache_if.icache icif,
+  caches_if.icache cif
 );
 
   logic ihit;
   assign icif.ihit = ihit;
+  ramstate_t istate;
   icachef_t instr_cache_address;
   assign instr_cache_address.tag = icif.imemaddr[31:6];
   assign instr_cache_address.idx = icif.imemaddr[5:2];
@@ -29,16 +31,24 @@ module icache (
        instr_cache[instr_cache_address.idx] <= nxt_icache_frame;
     end
   end
-   
-  // inputs:  imemREN, imemaddr
-  // outputs: ihit, imemload
 
   always_comb begin
     if ( (instr_cache[instr_cache_address.idx].tag == instr_cache_address.tag) && (instr_cache[instr_cache_address].valid) ) begin
+      cif.iaddr = word_t'(0);
+      cif.iREN = 1'b0;
       ihit = 1'b1;
+      nxt_icache_frame = instr_cache[instr_cache_address.idx]; 
     end
     else begin
+      cif.iaddr = icif.imemaddr;
+      cif.iREN = 1'b1;
       ihit = 1'b0;
+      if (cif.iwait == 1'b0) begin
+	nxt_icache_frame = {1'b1, instr_cache[instr_cache_address.idx].tag, cif.iload};
+      end
+      else begin
+	nxt_icache_frame = instr_cache[instr_cache_address.idx]; 
+      end
     end
   end
   
