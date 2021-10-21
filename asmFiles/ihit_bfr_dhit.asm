a51583944af9dfc7248fd92fbea11478928a12d8
Nate Nauman <nnauman@purdue.edu>
2021-10-21 11:46:01 -0400

    finished unit tests

--- asmFiles/ihit_bfr_dhit.asm
+++ asmFiles/ihit_bfr_dhit.asm
@@ -3,10 +3,12 @@
 
   addi $s0, $0, 0x7
   addi $a0, $0, 0x0224
-  addi $s1, $0, 0xF
-  sw   $s0, 0($a0)
+  addi $s1, $0, 0x9
 LOOP:	
   addi $s0, $s0, 0x1
+  sw   $s0, 0($a0)
+  addi $a0, $a0, 0x8
   bne  $s0, $s1, LOOP
-  lw   $s2, 0($a0)
+  addi $a1, $0, 0x0224
+  lw   $s1, 8($a1)
   halt
